6a173af3a4e6ac7e620a74f30b8153a4ff9ec4b5
eragefe <georgios@gdis.se>
2021-02-08 16:23:59 +0100

    new

--- web/app.py
+++ web/app.py
@@ -31,6 +31,23 @@ def save_credentials():
     os.system('mv wifi.tmp /root/wifi')
     os.system('sed -i "$ i bash /root/startwifi" /etc/rc.local')
     os.system('bash /root/wifi')
+    return ('', 204)
+
+@app.route("/volume", methods = ['GET', 'POST'])
+def volume():
+    a = request.form["a"]
+    create_file(a)
+    os.system('cp vol.tmp /root/vol')
+    os.system('bash /root/volweb')
+    with open("/root/vol", "r") as f:
+         vol = f.read()
+    return ('', 204)
+
+def create_file(a):
+
+    temp_conf_file = open('vol.tmp', 'w')
+    temp_conf_file.write('' + a + '')
+    temp_conf_file.close
 
 @app.route('/input')
 def input():
@@ -51,27 +68,25 @@ def power():
 @app.route('/net')
 def net():
     os.system('bash /root/net')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/nos')
 def nos():
     os.system('i2cset -y 1 17 5 1')
-    return render_template('sound.html')
+    return render_template('nos.html')
 
 @app.route('/sound1')
 def sound1():
     os.system('i2cset -y 1 17 5 0')
     os.system('i2cset -y 1 17 8 4')
-    return render_template('sound.html')
+    return render_template('sound1.html')
 
 @app.route('/sound2')
 def sound2():
     os.system('i2cset -y 1 17 8 0')
-    return render_template('sound.html')
+    return render_template('sound2.html')
 
-@app.route('/¨volup', methods = ['GET', 'POST'])
+@app.route('/volup', methods = ['GET', 'POST'])
 def volup():
     os.system('bash /root/volup')
     with open("/root/vol", "r") as f:
@@ -89,81 +104,63 @@ def voldown():
 def reboot():
     time.sleep(1)
     os.system('reboot')
+    return ('', 204)
 
 @app.route('/poweroff', methods = ['GET', 'POST'])
 def poweroff():
     time.sleep(1)
     os.system('poweroff')
+    return ('', 204)
 
 @app.route('/streamer', methods = ['GET', 'POST'])
 def streamer():
     os.system('bash /root/streamer')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/optical1', methods = ['GET', 'POST'])
 def optical1():
     os.system('bash /root/optical1')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/optical2', methods = ['GET', 'POST'])
 def optical2():
     os.system('bash /root/optical2')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/coaxial1', methods = ['GET', 'POST'])
 def coaxial1():
     os.system('bash /root/coaxial1')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/coaxial2', methods = ['GET', 'POST'])
 def coaxial2():
     os.system('')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/s1', methods = ['GET', 'POST'])
 def s1():
     os.system('bash /root/test')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/prev', methods = ['GET', 'POST'])
 def prev():
     os.system('mpc prev')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/play', methods = ['GET', 'POST'])
 def play():
     os.system('mpc toggle')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/stop', methods = ['GET', 'POST'])
 def stop():
     os.system('mpc stop')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 @app.route('/next', methods = ['GET', 'POST'])
 def next():
     os.system('mpc next')
-    with open("/root/vol", "r") as f:
-         vol = f.read()
-    return render_template('app.html', vol=vol)
+    return ('', 204)
 
 ######## FUNCTIONS ##########
 
